71175877f15c234d4c21b1aca133b0d989b26c6d
Dmalex166 <152544901+Dmalex166@users.noreply.github.com>
2024-07-23 16:52:57 -0400

    Movement controls work
    Jump and left and right movement added, bugs to fix are running into wall stops gravity & can't quite walk properly on tiles

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,40 +6,59 @@ public class PlayerMove : MonoBehaviour
 {      
     Rigidbody2D rb; 
     //Jump values
-    [SerializeField] int JumpStrength;
+    [SerializeField] int jumpStrength;
     [SerializeField] float fastFall;
+    [SerializeField] float moveSpeed;
+   
     
     Vector2 grav;
     //inputs
-    public InputAction inputAction;
-    public InputAction Left;
+    public InputAction up;
+    public InputAction left;
+    public InputAction right;
    
     public Transform groundCheck;
     public LayerMask groundlevel;
-
+    //checks if player is on ground
     bool onGround(){
         return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1.0f, 0.1f), CapsuleDirection2D.Horizontal,0,groundlevel);
     }
     void Start()
     {
-        inputAction.Enable();
-        RightLeft.Enable();
+        up.Enable();
+        left.Enable();
+        right.Enable();
         rb = GetComponent<Rigidbody2D>();
         grav = new Vector2(0, -Physics2D.gravity.y);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
-        if(inputAction.IsPressed() && onGround()){
-            rb.velocity = new Vector2(rb.velocity.x, JumpStrength);
+        if(up.IsPressed() && onGround()){
+            rb.velocity = new Vector2(rb.velocity.x, jumpStrength);
+            
         }
+        
         if(rb.velocity.y < 0){
             rb.velocity -= grav * fastFall * Time.deltaTime;
+        }
+        
+        if(left.IsPressed()){
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+         } else if (!(left.IsPressed()) && !(right.IsPressed())) {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+
+        if(right.IsPressed()){
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        } else if (!(left.IsPressed()) && !(right.IsPressed())) {
+            rb.velocity = new Vector2(0, rb.velocity.y);
         }
 
     }
 }
-    //checks player is grounded before allowing jumps
+    
     
